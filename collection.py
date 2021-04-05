039b8d2906e848d41d7a6048118a17545e5a6a2c
nicofarr1981 <nicofarr@gmail.com>
2021-04-05 09:45:22 -0500

    complete exercise

--- collection.py
+++ collection.py
@@ -2,15 +2,9 @@ import sys
 
 if __name__ == '__main__':
     t = int(sys.stdin.readline())
-    #for i in range(t):
-    lenA = 0
-    A = []
-    lenB = 0
-    B = []
-    lenA = int(sys.stdin.readline())
-    A = set(map(int,sys.stdin.readline().split())))
-    lenB = int(sys.stdin.readline())
-    B = set(map(int,sys.stdin.readline().split())))
-    print(A)
-    print(b)
-
+    for i in range(t):
+        lenA = int(sys.stdin.readline())
+        A = set(map(int,sys.stdin.readline().split()))
+        lenB = int(sys.stdin.readline())
+        B = set(map(int,sys.stdin.readline().split()))
+        print(A.issubset(B))
