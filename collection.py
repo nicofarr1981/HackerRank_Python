ee45d933e8a6c680d69ccbe300bad36019264aea
nicofarr1981 <81972941+nicofarr1981@users.noreply.github.com>
2021-04-05 09:06:04 -0500

    Collection challenge

---
+++ collection.py
@@ -0,0 +1,16 @@
+import sys
+
+if __name__ == '__main__':
+    t = int(sys.stdin.readline())
+    #for i in range(t):
+    lenA = 0
+    A = []
+    lenB = 0
+    B = []
+    lenA = int(sys.stdin.readline())
+    A = set(map(int,sys.stdin.readline().split())))
+    lenB = int(sys.stdin.readline())
+    B = set(map(int,sys.stdin.readline().split())))
+    print(A)
+    print(b)
+
